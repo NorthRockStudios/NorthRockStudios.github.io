d9bfa016a0601d8d53aa91af67c03de41c95b6f4
NorthRockStudios <108765562+NorthRockStudios@users.noreply.github.com>
2026-07-29 04:59:17 -0400

    More Card Details Changes

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const PROJECTS = [
   {
     title: "Modular UObject Components",
     tag: "Data-driven actor composition controlled through FGameplayTag()'s",
-    stat: "Components replace inheritance",
+    stat: "0 subclasses per game mode",
     video: "ZQQfKKbMuiA",
     problem:
       "Core AActors require new types for each Game Mode. Ex: A new AGameMode, APlayerController, and so on per mode. This doesn't scale and it forces hard-coded behavior into inheritance.",
@@ -21,9 +21,9 @@ const PROJECTS = [
     source: null,
   },
   {
-    title: "Load Balanced ID Driven Data",
-    tag: "A tag-addressable DataTable registry where any row is accessible using a 4-byte Id.",
-    stat: "≈ 95% less net bandwidth",
+    title: "Load Balanced & <br>ID Driven Data",
+    tag: "An ID addressable DataTable registry where any row is accessible using its unique 4-byte ID.",
+    stat: "≈90% less net bandwidth",
     video: null,
     problem:
       "Replicating a reference to data-driven content (Ex. an item or an ability) normally requires replication of a soft object path or NetGUID, and it grows with the payload.",
@@ -31,13 +31,13 @@ const PROJECTS = [
       "A globally accessible DataTable registry addressable via FGameplayTag() or FGlobalDataID() lets any system fetch any table or row from anywhere, no hard references. Each row is identified by a 4-byte handle struct that encodes table Id + row Id. Clients resolve the handle locally to the exact row; heavy data never travels.  The DataTable is also managed by a load balancer that keeps active DT's alive in memory longer and automatically unloads DT's that aren't within a certain period of time.",
     result:
       "A constant 4 bytes per reference regardless of payload size — roughly 95% smaller than soft object path replication and requires little to no CPU cycles to Net Serialize.",
-    tags: ["C++", "NetSerialize", "DataTables", "Gameplay Tags", "Bandwidth"],
+    tags: ["C++", "NetSerialize", "DataTables", "GameplayTags", "Bandwidth"],
     source: null,
   },
   {
     title: "Simplified Input Management",
-    tag: "Centralized input handling via priority layering. Allows any system to Add/Remove event/s to an input/s without having to touch the EnhancedInputSystem.",
-    stat: "No Tick() required",
+    tag: "Centralized input handling via priority layering that allows any system to Add/Remove event/s to an input/s.",
+    stat: "Tick-less Input Handling",
     video: null,
     problem:
       "EnhancedInput can be a complicated mess to Add/Remove input events to - requiring Input Actions, Mappings, and access to the local players InputComponent.",
@@ -45,13 +45,13 @@ const PROJECTS = [
       "Move input control over to a globally accessible subsystem that can be accessed where ever we have a world context object. This system will receive input directly from our GameInstance object and will be a level below EnhancedInput and actually control which inputs are fed into EnhancedInput after our priority check.",
     result:
       "Easy to manage input events from anywhere that gives us complete control over raw inputs from the player. This also reduces the cost of reading each input by allowing us to add/remove blacklisted keybinds that are completely disregarded.",
-    tags: ["C++","Input routing", "Enhanced Input"],
+    tags: ["C++", "Input", "EnhancedInput"],
     source: null,
   },
   {
-    title: "Automated UI Layout & Layering",
-    tag: "A faster CommonUI-style framework",
-    stat: "Automatic UI sizing/layering",
+    title: "Automated UI <br>Layout & Layering",
+    tag: "A custom CommonUI-style framework that allows all UI elements to be uniformly layered and placed.",
+    stat: "Zero manual layout/layering",
     video: null,
     problem:
       "The UMG designer hits limits for input routing and automating UI layering. I wanted full control at the lowest level possible to have my UI manage itself.",
@@ -59,26 +59,26 @@ const PROJECTS = [
       "Build an automated priority grid-based layer management that displays and routes inputs based on highest priority layer. Create a Base UI object type that automatically converts the UI's size to the Rows/Cols the UI occupies along with a layer enum that determines if its activateable along with which layer it's placed on.",
     result:
       "Completely automated UI input routing, layering, and placement that uses a grid based system similar to our Inventory's Grid system. UI will be placed within an active layer so long as the layer has space for it's required Row/Col amount.",
-    tags: ["C++", "Slate", "UMG", "Input routing"],
+    tags: ["C++", "Slate", "UMG", "Input"],
     source: null,
   },
   {
-    title: "FAS Inventory Grid System",
-    tag: "Multiple inventory types over Fast Array Serialization, each item uses our ID'ing UDataTable system.",
-    stat: "Efficient net delta sync",
+    title: "FAS Grid-based <br>Inventory System",
+    tag: "Multiple inventory types using Fast Array Serialization, and each item using our Data ID system.",
+    stat: "Efficient delta net serialization",
     video: null,
     problem:
-      "UPROPERTY(Replicated) TArray<> inventory replication not only resends the entire container for every change it also ships full item structs across the network which annihilates bandwidth and overworks our server's CPU.",
+      "UPROPERTY(Replicated) TArray<> replication resends the entire container for every change which also causes each item to re-serialie and ship across the network. This annihilates player bandwidth and overworks our server's CPU.",
     approach:
-      "A modular base that supports equipment, crafting, storage, and other inventory types. State replicates via Fast Array Serialization, so only items marked dirty go out — and each item uses our 4-byte ID handle so item details like name, icon, stats, etc. stay off the network and resolve locally.",
+      "A modular base inventory FAS that supports equipment, crafting, storage, and other inventory types. State replicates via Fast Array Serialization, so only items marked dirty serialize and ship. On top of this each item uses our 4-byte ID handle so item details like name, icon, stats, etc. stay off the network and resolve locally.",
     result:
-      "An inventory item's details and placement update costs roughly 6 bytes per item (outside of RPC header costs) since we completely avoid replicating any costly strings and allow clients to resolve item details locally from our DataTables.",
+      "An inventory that only replicates when marked dirty and whose item data costs, like details and placement info, are reduced from 100+ bytes to roughly 6 bytes per item (outside of RPC costs). This is accomplished by avoiding replication on strings/object paths/etc. and pushing the work onto clients to resolve item details locally from our DataTables.",
     tags: ["C++", "Fast Array Serialization", "Replication", "DataTables", "InstancedStruct"],
     source: null,
   },
   {
     title: "Editor Tooling",
-    tag: "Slate based Editor UI that make modifying Editor based data, like UDataTable's, more streamline.",
+    tag: "Slate based Editor UI that makes modifying custom struct property data more streamline.",
     stat: "Designer friendly editor UI",
     video: null,
     problem:
